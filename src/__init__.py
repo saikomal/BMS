2ad0285afbe08411889d0bce8c40c8bfdeba341d
saikomal <saikomalpendela@yahoo.com>
2020-07-11 17:22:50 +0530

    Add api for Updating company details and image id check when getting images

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from .actions.CompanyAction import company
 from .actions.CategoryAction import category
 from src.Configurations.MongoClient import bmsdb, fs, companyDB
 from bson.objectid import ObjectId
+from src.utils.UsualUtil import send_response
 
 app = Flask(__name__)
 app.register_blueprint(company)
@@ -11,7 +12,10 @@ app.register_blueprint(category)
 
 @app.route("/image/<imageid>", methods=["GET"])
 def getimage(imageid):
-    print(imageid)
-    image = fs.get(ObjectId(imageid))
-    print(image)
-    return send_file(image, mimetype="image/png")
+    if not ObjectId.is_valid(imageid):
+        return send_response(False, msg="Not a valid image id")
+    if fs.exists(ObjectId(imageid)):
+        image = fs.get(ObjectId(imageid))
+        return send_file(image, mimetype="image/png")
+    else:
+        return send_response(False, msg="Image not available")
--- src/actions/CompanyAction.py
+++ src/actions/CompanyAction.py
@@ -4,10 +4,11 @@ from src.utils.UsualUtil import send_response
 
 company = Blueprint('company', __name__, url_prefix="/company")
 
+companycollec = bmsdb[companyDB]
+
 
 @company.route('', methods=['GET'])
 def getCompanyDetails():
-    companycollec = bmsdb[companyDB]
     details = companycollec.find_one()
     details['logo'] = str(details['logo'])
     del details['_id']
@@ -17,7 +18,6 @@ def getCompanyDetails():
 @company.route('', methods=['POST'])
 def addCompanyDetails():
     try:
-        companycollec = bmsdb[companyDB]
         if companycollec.count() >= 1:
             return send_response(False, msg="You already have a company dude"), 409
         companyname = request.form.get('companyname')
@@ -27,18 +27,29 @@ def addCompanyDetails():
         if 'companylogo' in request.files:
             logo = request.files.get('companylogo')
             logo_id = fs.put(logo)
-            fs.put()
         description = request.form.get('description', "")
         data = {'companyname': companyname, 'logo': logo_id, 'description': description}
         companycollec.insert_one(data)
         return send_response(True, msg="Data entered"), 200
     except Exception as e:
-        return {'status': 'failed due to some error, Try again if you are patient'}, 500
+        return send_response(False,msg="failed due to some error, Try again if you are patient"), 500
 
 
 @company.route('', methods=['PUT'])
 def updateCompanyDetails():
-    return bmsdb.list_collection_names()[0]
+    try:
+        details = companycollec.find_one()
+        data = request.form.to_dict()
+        if 'companylogo' in request.files:
+            logo = request.files.get('companylogo')
+            fs.delete(details['logo'])
+            logo_id = fs.put(logo)
+            data['logo'] = logo_id
+        newvalues = {"$set": data}
+        companycollec.update_one(details, newvalues)
+        return send_response(True, msg="Data updated"), 200
+    except Exception as e:
+        return send_response(False,msg="failed due to some error, Try again if you are patient"), 500
 
 
 @company.route('', methods=["DELETE"])
