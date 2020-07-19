82d41f2e5e9241f67a6819d4998eda459e09a94c
srikanthy97 <67978138+srikanthy97@users.noreply.github.com>
2020-07-19 15:28:31 +0530

    Resolved some issues, Added Get_requested_customer_details feture

--- src/actions/CustomerAction.py
+++ src/actions/CustomerAction.py
@@ -1,7 +1,8 @@
 from src.Configurations.MongoClient import bmsdb, fs, customerDB
 from flask import Blueprint, request
 from src.utils.UsualUtil import send_response
-from src.utils.CustomerUtil import checkph
+from src.utils.CustomerUtil import user_exists
+from src.utils.CustomerUtil import get_specified_customer_details
 
 customer = Blueprint('customer', __name__, url_prefix="/customer")
 
@@ -10,18 +11,23 @@ custcollec = bmsdb[customerDB]
 @customer.route('', methods=['GET'])
 def get_customer_details():
     details = list(custcollec.find())
-    print(details)
     if details is None:
         return send_response(True, msg="No customer available to get")
     return send_response(True, data=details)
+
+@customer.route('/get_requested_customer_details', methods=['GET'])
+def get_requested_customer_details():
+    custph = request.form.get('custphone')
+    if get_specified_customer_details(custph).count()==0:
+        return send_response(True, msg="No requested customer available to get")
+    return send_response(True, data=list(get_specified_customer_details(custph)))
 
 
 @customer.route('', methods=['PUT'])
 def update_customer_details():
     custph = request.form.get('custphone')
-    if(checkph(custph)):
-        return send_response(True, msg="No given customer avaliable "), 200
-        exit(0)
+    if not user_exists(custph):
+        return send_response(True, msg="No given customer avaliable to update"), 200
     details = {"_id": custph}
     try:
         data = request.form.to_dict()
@@ -35,9 +41,8 @@ def update_customer_details():
 @customer.route('', methods=['DELETE'])
 def delete_customer_details():
     custph = request.form.get('custphone')
-    if(checkph(custph)):
-        return send_response(True, msg="No given customer avaliable "), 200
-        exit(0)
+    if not user_exists(custph):
+        return send_response(True, msg="No given customer avaliable to delete"), 200
     details={"_id" : custph}
     custcollec.delete_one(details)
     return send_response(True, msg="customer deleted"), 200
@@ -54,9 +59,10 @@ def add_customer_details():
         print(custph)
         if(custph == None):
             return send_response(True, msg="phoneno must be given"), 200
-            exit(0)
+        if user_exists(custph):
+            return send_response(True, msg="Given customer already exists"), 200
         custcollec.insert_one(data)
         return send_response(True, msg="Data entered"), 200
     except Exception as e:
-        return send_response(False, msg="failed due to sdfdsome error, Try again if you are patient"), 500
+        return send_response(False, msg="failed due to sdfdsome error, Try again if you are patient"),
 
--- src/utils/CustomerUtil.py
+++ src/utils/CustomerUtil.py
@@ -1,9 +1,9 @@
 from src.Configurations.MongoClient import bmsdb, fs, customerDB
 custcollec = bmsdb[customerDB]
-def checkph(custph):
-    details = list(custcollec.find({"_id":custph}, {'_id': False}))
-    if(len(details)==0):
-        return True
-    else:
-        return False
+
+def user_exists(custph):
+    return True if custcollec.find({"_id":custph}, {}).count()==1 else False
+
+def get_specified_customer_details(custph):
+    return custcollec.find({"_id": custph})
 
