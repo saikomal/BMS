29b52513ce96f73a47cc9b0979cfd6f6e4c03c62
srikanthy97 <67978138+srikanthy97@users.noreply.github.com>
2020-07-22 17:35:18 +0530

    Added adits

--- src/actions/CustomerAction.py
+++ src/actions/CustomerAction.py
@@ -5,6 +5,13 @@ from src.utils.CustomerUtil import user_exists
 from src.utils.AuditUtil import audit
 from src.Configurations.CustomerTable import CustomerTable
 from src.utils.CustomerUtil import get_specified_customer_details
+from src.utils.AuditUtil import audit
+
+GET_CUSTOMER_OPERATION = "GET CUSTOMER DETAILS"
+GET_CUSTOMER_OPERATION1 = "GET REQUESTED CUSTOMER DETAILS"
+ADD_CUSTOMER_OPERATION = "ADD CUSTOMER DETAILS"
+DELETE_CUSTOMER_OPERATION = "DELETE CUSTOMER DETAILS"
+UPDATE_CUSTOMER_OPERATION = "UPDATE CUSTOMER DETAILS"
 
 customer = Blueprint('customer', __name__, url_prefix="/customer")
 
@@ -13,6 +20,7 @@ custcollec = bmsdb[CustomerTable.collecname]
 
 @customer.route('', methods=['GET'])
 def get_customer_details():
+    ip_addr = request.remote_addr
     sin = int(request.form.get("sin","0"))
     no_of_rows = int(request.form.get("no_of_rows","50"))
     if sin<0:
@@ -22,25 +30,32 @@ def get_customer_details():
     else:
         details = list(custcollec.find().skip(sin).limit(no_of_rows))
     if len(details)==0:
+        audit(operation_type=GET_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(True, msg="No customer available to get"), 200
+    audit(operation_type=GET_CUSTOMER_OPERATION, performed_by=ip_addr)
     return send_response(True, data={"customer_details":details,"total_count":custcollec.count()})
 
 
 @customer.route('/get_requested_customer_details/<custph>', methods=['GET'])
 def get_requested_customer_details(custph):
+    ip_addr = request.remote_addr
     if (custph == None):
         return send_response(True, msg="phoneno must be given"), 200
     try:
         custph1 = int(custph)
     except:
+        audit(operation_type=GET_CUSTOMER_OPERATION1, performed_by=ip_addr)
         return send_response(False,msg="Invalid number")
     if get_specified_customer_details(custph).count() == 0:
+        audit(operation_type=GET_CUSTOMER_OPERATION1, performed_by=ip_addr)
         return send_response(True, msg="No requested customer available to get"), 200
+    audit(operation_type=GET_CUSTOMER_OPERATION1, performed_by=ip_addr)
     return send_response(True, data=list(get_specified_customer_details(custph)))
 
 
 @customer.route('', methods=['PUT'])
 def update_customer_details():
+    ip_addr = request.remote_addr
     data={}
     data.update(customername =request.form.get('customername'))
     data.update(custadd=request.form.get('customeradd'))
@@ -49,37 +64,47 @@ def update_customer_details():
     data.update(points=request.form.get('points'))
     custph=request.form.get('custphone')
     if (custph == None):
+        audit(operation_type=UPDATE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(True, msg="phoneno must be given"), 200
     if not user_exists(custph):
+        audit(operation_type=UPDATE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(True, msg="No given customer available to update"), 200
     try:
         custph1 = int(custph)
     except:
+        audit(operation_type=UPDATE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(False,msg="Invalid number")
     details = {"_id": custph}
     try:
         newvalues = {"$set": data}
         custcollec.update_one(details, newvalues)
+        audit(operation_type=UPDATE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(True, msg="Data updated"), 200
     except Exception as e:
+        audit(operation_type=UPDATE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(False, msg="failed due to some error, Try again if you are patient"), 500
 
 
 @customer.route('<custph>', methods=['DELETE'])
 def delete_customer_details(custph):
+    ip_addr = request.remote_addr
     if not user_exists(custph):
+        audit(operation_type=DELETE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(True, msg="No given customer avaliable to delete"), 200
     try:
         custph1 = int(custph)
     except:
+        audit(operation_type=DELETE_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(False,msg="Invalid number")
     details = {"_id": custph}
     custcollec.delete_one(details)
+    audit(operation_type=DELETE_CUSTOMER_OPERATION, performed_by=ip_addr)
     return send_response(True, msg="customer deleted"), 200
 
 
 @customer.route('', methods=['POST'])
 def add_customer_details():
+    ip_addr = request.remote_addr
     try:
         custname = request.form.get('customername')
         custadd = request.form.get('customeradd')
@@ -89,14 +114,18 @@ def add_customer_details():
         data = {'customername': custname, 'customeradd': custadd, '_id': custph, 'comments': comments, 'points': points}
         print(custph)
         if (custph == None):
+            audit(operation_type=ADD_CUSTOMER_OPERATION, performed_by=ip_addr)
             return send_response(True, msg="phoneno must be given"), 200
         if user_exists(custph):
+            audit(operation_type=ADD_CUSTOMER_OPERATION, performed_by=ip_addr)
             return send_response(True, msg="Given customer already exists"), 200
         try:
             custph1 = int(custph)
         except:
+            audit(operation_type=ADD_CUSTOMER_OPERATION, performed_by=ip_addr)
             return send_response(False, msg="Invalid number")
         custcollec.insert_one(data)
         return send_response(True, msg="Data entered"), 200
     except Exception as e:
+        audit(operation_type=ADD_CUSTOMER_OPERATION, performed_by=ip_addr)
         return send_response(False, msg="failed due to sdfdsome error, Try again if you are patient"), 500
