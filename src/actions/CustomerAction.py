368daae3de245d0a61812160bbafee2293f3966c
srikanthy97 <67978138+srikanthy97@users.noreply.github.com>
2020-07-22 17:03:36 +0530

    Added phone check for integer Added total count in get customer details

--- src/actions/CustomerAction.py
+++ src/actions/CustomerAction.py
@@ -11,25 +11,29 @@ customer = Blueprint('customer', __name__, url_prefix="/customer")
 custcollec = bmsdb[CustomerTable.collecname]
 
 
-# custcollec.createIndex( { "custphone": 1 },{unique:true})
 @customer.route('', methods=['GET'])
 def get_customer_details():
-    #details = list(custcollec.find())
-    sin = request.form.get("sin")
-    no_of_rows = request.form.get("no_of_rows")
-    if int(sin)<0:
+    sin = int(request.form.get("sin","0"))
+    no_of_rows = int(request.form.get("no_of_rows","50"))
+    if sin<0:
         sin=0
-    if int(no_of_rows)<=0:
-        no_of_rows=0
-    details = list(custcollec.find().skip(int(sin)).limit(int(no_of_rows)))
+    if no_of_rows==-1:
+        details = list(custcollec.find().skip(sin))
+    else:
+        details = list(custcollec.find().skip(sin).limit(no_of_rows))
     if len(details)==0:
         return send_response(True, msg="No customer available to get"), 200
-    return send_response(True, data=details)
+    return send_response(True, data={"customer_details":details,"total_count":custcollec.count()})
 
 
-@customer.route('/get_requested_customer_details', methods=['GET'])
-def get_requested_customer_details():
-    custph = request.form.get('custphone')
+@customer.route('/get_requested_customer_details/<custph>', methods=['GET'])
+def get_requested_customer_details(custph):
+    if (custph == None):
+        return send_response(True, msg="phoneno must be given"), 200
+    try:
+        custph1 = int(custph)
+    except:
+        return send_response(False,msg="Invalid number")
     if get_specified_customer_details(custph).count() == 0:
         return send_response(True, msg="No requested customer available to get"), 200
     return send_response(True, data=list(get_specified_customer_details(custph)))
@@ -37,12 +41,23 @@ def get_requested_customer_details():
 
 @customer.route('', methods=['PUT'])
 def update_customer_details():
-    custph = request.form.get('custphone')
+    data={}
+    data.update(customername =request.form.get('customername'))
+    data.update(custadd=request.form.get('customeradd'))
+    data.update(custph=request.form.get('custphone'))
+    data.update(comments=request.form.get('comments'))
+    data.update(points=request.form.get('points'))
+    custph=request.form.get('custphone')
+    if (custph == None):
+        return send_response(True, msg="phoneno must be given"), 200
     if not user_exists(custph):
-        return send_response(True, msg="No given customer avaliable to update"), 200
+        return send_response(True, msg="No given customer available to update"), 200
+    try:
+        custph1 = int(custph)
+    except:
+        return send_response(False,msg="Invalid number")
     details = {"_id": custph}
     try:
-        data = request.form.to_dict()
         newvalues = {"$set": data}
         custcollec.update_one(details, newvalues)
         return send_response(True, msg="Data updated"), 200
@@ -50,11 +65,14 @@ def update_customer_details():
         return send_response(False, msg="failed due to some error, Try again if you are patient"), 500
 
 
-@customer.route('', methods=['DELETE'])
-def delete_customer_details():
-    custph = request.form.get('custphone')
+@customer.route('<custph>', methods=['DELETE'])
+def delete_customer_details(custph):
     if not user_exists(custph):
         return send_response(True, msg="No given customer avaliable to delete"), 200
+    try:
+        custph1 = int(custph)
+    except:
+        return send_response(False,msg="Invalid number")
     details = {"_id": custph}
     custcollec.delete_one(details)
     return send_response(True, msg="customer deleted"), 200
@@ -74,6 +92,10 @@ def add_customer_details():
             return send_response(True, msg="phoneno must be given"), 200
         if user_exists(custph):
             return send_response(True, msg="Given customer already exists"), 200
+        try:
+            custph1 = int(custph)
+        except:
+            return send_response(False, msg="Invalid number")
         custcollec.insert_one(data)
         return send_response(True, msg="Data entered"), 200
     except Exception as e:
