ff70e67e28d1ddd11830f43d0b445c13524e8d76
saikomal <saikomalpendela@yahoo.com>
2020-07-24 16:18:27 +0530

    fix purchase issues in previous commit

--- src/Configurations/PurchaseTable.py
+++ src/Configurations/PurchaseTable.py
@@ -1,3 +1,4 @@
 class PurchaseTable:
+    collec_name="Purchases"
     items = "items"
     time_stamp = "time"
--- src/actions/PurchaseAction.py
+++ src/actions/PurchaseAction.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request
 from src.Configurations.MongoClient import bmsdb
-from src.Configurations.CategoryTable import CategoryTable
+from src.Configurations.PurchaseTable import PurchaseTable
 from src.utils.CategoryUtil import category_exists, valid_cat_id, valid_parent_for_category, get_all_children
 from src.utils.UsualUtil import send_response
 from src.utils.CompanyUtil import company_exists
 from bson import ObjectId
 
-purchase = Blueprint('category', __name__, url_prefix="/purchase")
-pur_collec = bmsdb[purchase]
+purchase = Blueprint('purchase', __name__, url_prefix="/purchase")
+pur_collec = bmsdb[PurchaseTable.collec_name]
