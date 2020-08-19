eedc46bc9ad415be9faf0c19c401dde3ce520000
Sai Komal <saikomalpendela@gmail.com>
2020-08-19 19:48:04 +0530

    form input in admin page with redux-form

--- react-client/src/components/admin/Admin.js
+++ react-client/src/components/admin/Admin.js
@@ -1,7 +1,83 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-const Admin = () => {
-  return <div> Admin Page </div>;
+const Admin = (props) => {
+  const parentRef = React.createRef();
+  const imageRef = React.createRef();
+
+  const handleChange = (event, input) => {
+    event.preventDefault();
+    console.log(event, input);
+    let imageFile = event.target.files[0];
+    if (imageFile) {
+      const localImageUrl = URL.createObjectURL(imageFile);
+      imageRef.current.src = localImageUrl;
+      input.onChange(imageFile);
+    }
+  };
+  const renderInput = ({ input, type, style, label }) => {
+    if (input.name === "image") {
+      delete input.value;
+      return (
+        <input
+          {...input}
+          type="file"
+          ref={parentRef}
+          style={style}
+          onChange={(event) => handleChange(event, input)}
+        />
+      );
+    } else {
+      return (
+        <div className="field">
+          <label>{label}</label>
+          <input {...input} type={type} />
+        </div>
+      );
+    }
+  };
+
+  const postCompanyDetails = (data) => {
+    console.log("_+_+_+");
+    console.log(data);
+  };
+
+  return (
+    <form className="ui form" onSubmit={props.handleSubmit(postCompanyDetails)}>
+      <Field
+        name="name"
+        component={renderInput}
+        type="text"
+        label="Company Name"
+      />
+      <Field
+        type="text"
+        name="description"
+        component={renderInput}
+        label="Description"
+      />
+      <div className="field">
+        <div className="ui small image">
+          <img
+            src="https://www.metatube.com/assets/metatube/video/img/Upload.svg"
+            alt="Click to upload"
+            onClick={() => parentRef.current.click()}
+            ref={imageRef}
+          />
+        </div>
+        <Field
+          name="image"
+          style={{ display: "none" }}
+          component={renderInput}
+        />
+      </div>
+      <button className="ui button primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
-export default Admin;
+export default reduxForm({
+  form: "companyDetails",
+})(Admin);
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import Header from "./components/Header";
@@ -10,19 +12,27 @@ import Analytics from "./components/Analytics";
 import Cart from "./components/Cart";
 
 import history from "./history";
+import reducers from "./reducers";
+
+const composeEnchancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
-      <Header />
-      <div>
-        <Route path="/" exact component={App} />
-        <Route path="/admin" exact component={Admin} />
-        <Route path="/customers" exact component={Customers} />
-        <Route path="/analytics" exact component={Analytics} />
-        <Route path="/cart" exact component={Cart} />
-      </div>
-    </Router>
+    <Provider
+      store={createStore(reducers, composeEnchancers(applyMiddleware()))}
+    >
+      <Router history={history}>
+        <Header />
+        <div>
+          <Route path="/" exact component={App} />
+          <Route path="/admin" exact component={Admin} />
+          <Route path="/customers" exact component={Customers} />
+          <Route path="/analytics" exact component={Analytics} />
+          <Route path="/cart" exact component={Cart} />
+        </div>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
